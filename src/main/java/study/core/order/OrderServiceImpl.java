b5dd30c803708f057c223a0756484a87d98e85e3
since1909 <since1909@daum.net>
2022-05-01 21:33:56 +0900

    Add: order and discount policy domain

---
+++ src/main/java/study/core/order/OrderServiceImpl.java
@@ -0,0 +1,21 @@
+package study.core.order;
+
+import study.core.discount.DiscountPolicy;
+import study.core.discount.FixDiscountPolicy;
+import study.core.member.Member;
+import study.core.member.MemberRepository;
+import study.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService{
+
+     private final MemberRepository memberRepository = new MemoryMemberRepository();
+     private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);            //회원 정보 조회
+        int discountPrice = discountPolicy.discount(member, itemPrice); //할인 정책 조회
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
