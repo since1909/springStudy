2d95e5978d77e6df07a32b9922957ce7b01236e7
since1909 <since1909@daum.net>
2022-05-08 17:31:16 +0900

    Fix: ocp violation-client imple code change  sample code

--- src/main/java/study/core/order/OrderServiceImpl.java
+++ src/main/java/study/core/order/OrderServiceImpl.java
@@ -2,14 +2,16 @@ package study.core.order;
 
 import study.core.discount.DiscountPolicy;
 import study.core.discount.FixDiscountPolicy;
+import study.core.discount.RateDiscountPolicy;
 import study.core.member.Member;
 import study.core.member.MemberRepository;
 import study.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
 
-     private final MemberRepository memberRepository = new MemoryMemberRepository();
-     private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final DiscountPolicy discountPolicy = new RateDiscountPolicy(); //ocp 위반
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
