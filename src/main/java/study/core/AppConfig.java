d48935c70e51846cd0cb2957f3f3194afc96addb
since1909 <since1909@daum.net>
2022-05-08 17:57:48 +0900

    Add: AppConfig for DIP-constructor injection

---
+++ src/main/java/study/core/AppConfig.java
@@ -0,0 +1,22 @@
+package study.core;
+
+import study.core.discount.FixDiscountPolicy;
+import study.core.member.MemberService;
+import study.core.member.MemberServiceImpl;
+import study.core.member.MemoryMemberRepository;
+import study.core.order.OrderService;
+import study.core.order.OrderServiceImpl;
+
+public class AppConfig {
+
+    //constructor injection
+    public MemberService memberService() {
+        return new MemberServiceImpl(new MemoryMemberRepository());
+    }
+
+    public OrderService orderService() {
+        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    }
+
+
+}
--- src/main/java/study/core/order/OrderServiceImpl.java
+++ src/main/java/study/core/order/OrderServiceImpl.java
@@ -1,17 +1,18 @@
 package study.core.order;
 
 import study.core.discount.DiscountPolicy;
-import study.core.discount.FixDiscountPolicy;
-import study.core.discount.RateDiscountPolicy;
 import study.core.member.Member;
 import study.core.member.MemberRepository;
-import study.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-    private final DiscountPolicy discountPolicy = new RateDiscountPolicy(); //ocp 위반
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
