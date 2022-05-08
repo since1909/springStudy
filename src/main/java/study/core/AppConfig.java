045908fc6e1479e5a6d6c46d55854eab33e309b3
since1909 <since1909@daum.net>
2022-05-08 19:17:28 +0900

    Fix: convert to spring framework

--- src/main/java/study/core/AppConfig.java
+++ src/main/java/study/core/AppConfig.java
@@ -1,5 +1,7 @@
 package study.core;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import study.core.discount.DiscountPolicy;
 import study.core.discount.FixDiscountPolicy;
 import study.core.discount.RateDiscountPolicy;
@@ -10,23 +12,28 @@ import study.core.member.MemoryMemberRepository;
 import study.core.order.OrderService;
 import study.core.order.OrderServiceImpl;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
         //return new FixDiscountPolicy();
         return new RateDiscountPolicy();
     }
 
-}
+ }
--- src/main/java/study/core/OrderApp.java
+++ src/main/java/study/core/OrderApp.java
@@ -1,5 +1,7 @@
 package study.core;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import study.core.member.Grade;
 import study.core.member.Member;
 import study.core.member.MemberService;
@@ -11,9 +13,9 @@ import study.core.order.OrderServiceImpl;
 public class OrderApp {
     public static void main(String[] args) {
 
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         //가입
         Long memberId = 1L;
